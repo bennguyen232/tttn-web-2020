639d105ff4e049bcc40b5821e19a55ec6bc8c4d1
bennguyen232 <nguyenduythai427@gmail.com>
2020-08-06 20:05:43 +0700

    build: init project

--- src/app/hocs/onlyLogin.tsx
+++ src/app/hocs/onlyLogin.tsx
@@ -1,13 +1,30 @@
 import React, {Component, FC} from 'react';
+import {makeStyles, Theme, createStyles} from '@material-ui/core';
 
 const withMenu = (wrappedComponent: Component | FC) => {
   const WrappedComponent = wrappedComponent as any;
-  const hocComponent: FC = ({...props}) => (
-    <section>
-      <WrappedComponent {...props} />
-    </section>
-  );
-  return hocComponent;
+  const HocComponent: FC = ({...props}) => {
+    const classes = useStyles();
+    return (
+      <section className={classes.root}>
+        <WrappedComponent {...props} />
+      </section>
+    );
+  };
+  return HocComponent;
 };
 
 export default withMenu;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      background: theme.palette.background.default,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
